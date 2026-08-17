21d545592269b790817a52e4219ef4cc1686f5f7
Nguyễn Tường Bách Hỷ <121304768+nguyentuongbachhy@users.noreply.github.com>
2026-08-17 23:58:32 +0700

    remove unused profile re-export suppression

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -15,8 +15,7 @@ pub use fp8_analysis::Fp8GemmErrorReport;
 pub(crate) use fp8_analysis::characterize_gemm_site;
 pub(crate) use lfm2::BatchModelCache;
 pub use lfm2::Lfm2Model;
-#[allow(unused_imports)]
-pub use profile::{DecodeProfileMode, DecodeProfileReport, ProfileComponent};
+pub use profile::{DecodeProfileMode, DecodeProfileReport};
 pub(crate) use profile::{ModelProfileRecorder, ProfileRegion, profiled};
 pub use quantization::Fp8PrecisionPolicy;
 pub(crate) use quantization::PrecisionClass;
