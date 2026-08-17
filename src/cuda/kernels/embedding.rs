9b2b7024d89f5f1245c6ea78c11d0c6534d67d32
Nguyễn Tường Bách Hỷ <121304768+nguyentuongbachhy@users.noreply.github.com>
2026-08-18 00:05:50 +0700

    group embedding kernel launch arguments

--- src/cuda/kernels/embedding.rs
+++ src/cuda/kernels/embedding.rs
@@ -10,18 +10,25 @@ use super::kernel_set::KernelSet;
 
 const MAX_BLOCK_SIZE: u32 = 256;
 
+pub(crate) struct EmbeddingLaunch<'a> {
+    pub(crate) token_ids: &'a CudaSlice<u32>,
+    pub(crate) weight: &'a CudaSlice<bf16>,
+    pub(crate) out: &'a mut CudaSlice<bf16>,
+    pub(crate) num_tokens: usize,
+    pub(crate) vocab_size: usize,
+    pub(crate) hidden_size: usize,
+}
+
 pub(crate) struct EmbeddingKernels {
     embedding_bf16: KernelLaunch,
 }
 
 impl KernelSet for EmbeddingKernels {
     const MODULE_NAME: &'static str = "embedding";
-
     const PTX: &'static str = include_str!(concat!(env!("OUT_DIR"), "/embedding.ptx"));
 
     fn from_module(module: Arc<CudaModule>) -> Result<Self> {
         let function = load_function(&module, Self::MODULE_NAME, "embedding_bf16")?;
-
         Ok(Self {
             embedding_bf16: KernelLaunch::new(function, MAX_BLOCK_SIZE)?,
         })
@@ -32,28 +39,27 @@ impl EmbeddingKernels {
     pub(crate) unsafe fn launch_bf16(
         &self,
         stream: &CudaStream,
-        token_ids: &CudaSlice<u32>,
-        weight: &CudaSlice<bf16>,
-        out: &mut CudaSlice<bf16>,
-        num_tokens: usize,
-        vocab_size: usize,
-        hidden_size: usize,
+        launch: EmbeddingLaunch<'_>,
     ) -> Result<()> {
+        let EmbeddingLaunch {
+            token_ids,
+            weight,
+            out,
+            num_tokens,
+            vocab_size,
+            hidden_size,
+        } = launch;
         let config = self.embedding_bf16.policy().exact_blocks(num_tokens)?;
-
         let mut args = stream.launch_builder(self.embedding_bf16.function());
-
         args.arg(token_ids)
             .arg(weight)
             .arg(out)
             .arg(&num_tokens)
             .arg(&vocab_size)
             .arg(&hidden_size);
-
         unsafe {
             args.launch(config)?;
         }
-
         Ok(())
     }
 }
